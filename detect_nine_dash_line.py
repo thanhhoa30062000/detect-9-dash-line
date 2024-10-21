04ecf89d695bc5fe2b3424c2fc74cdd08687ac60
thanhhoa30062000 <thanhhoanganlun@gmail.com>
2024-10-21 21:22:55 +0700

    Update detect_nine_dash_line.py

--- detect_nine_dash_line.py
+++ detect_nine_dash_line.py
@@ -6,6 +6,7 @@ import numpy as np
 
 # Thiết lập đường dẫn tới model đã train
 model_path = 'runs/train/exp6/weights/best.pt'  # Thay đổi theo đường dẫn của bạn
+path_load = 'ultralytics/yolov5'
 # Tải model
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
 
@@ -13,6 +14,7 @@ st.title("DETECT NINE DASH-LINE")
 
 conf_thres = st.slider("Chọn ngưỡng độ tin cậy (Confidence Threshold)", 0.25, 0.5, 0.75)
 model.conf = conf_thres
+
 # Tải ảnh từ tệp
 uploaded_file = st.file_uploader("Select Image or Video", type=['jpg', 'jpeg', 'png', 'mp4'])
 
@@ -21,7 +23,7 @@ if uploaded_file is not None:
     if uploaded_file.type in ['image/jpeg', 'image/png']:
         image = Image.open(uploaded_file)
         st.image(image, caption='File Uploaded', use_column_width=True)
-
+        detected = False
         # Chuyển đổi ảnh thành định dạng phù hợp với YOLOv5
         img = np.array(image)
         results = model(img) 
@@ -29,14 +31,14 @@ if uploaded_file is not None:
         class_names = results.names
 
         if any(class_names[int(cls)] == '9_dash_line' for cls in detected_classes):
-            detected_heo = True
+            detected = True
 
-        if detected_heo:
-            st.write("**Discovered nine dash-line in this video!**")
-        else:
-            st.write("**Nothing**")
         # Hiển thị kết quả
         st.image(results.render()[0], caption='Detect', use_column_width=True)
+        if detected:
+            st.write("**Discovered nine dash-line in this image!**")
+        else:
+            st.write("**Nothing**")
 
     elif uploaded_file.type == 'video/mp4':
         st.video(uploaded_file)
@@ -48,24 +50,36 @@ if uploaded_file is not None:
         # Xử lý video
         vid = cv2.VideoCapture("temp_video.mp4")
         frame_count = 0
-        detected_heo = False
+        detected = False
+        first_detection_time = None  # Thời gian phát hiện đầu tiên
 
+        fps = vid.get(cv2.CAP_PROP_FPS)  # Lấy thông số FPS của video
+        count_print = False
         while True:
             ret, frame = vid.read()
             if not ret:
                 break
-            
+
             results = model(frame) 
             detected_classes = results.xyxyn[0][:, -1].numpy()
             class_names = results.names
 
             if any(class_names[int(cls)] == '9_dash_line' for cls in detected_classes):
-                detected_heo = True
+                if not detected:  # Chỉ ghi thời gian nếu chưa phát hiện trước đó
+                    first_detection_time = frame_count / fps
+                    detected = True
+                # Hiển thị kết quả
+                if not count_print:
+                    count_print = True
+                    st.image(results.render()[0], caption='Detect', use_column_width=True)
+
+            frame_count += 1
 
         vid.release()
         
-        if detected_heo:
+        if detected:
             st.write("**Discovered nine dash-line in this video!**")
+            st.write(f"**First detection at: {first_detection_time:.2f} seconds**")
         else:
             st.write("**Nothing**")
 
